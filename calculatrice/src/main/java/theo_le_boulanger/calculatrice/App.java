b1ed8f1ebfe3365b41be1c27333d2156ed7c840e
Theo <theoleboulanger@gmail.com>
2017-01-29 12:04:54 +0100

    Création du projet (Calculatrice en notation polonaise inversée)
    * Configuration de Maven
    * Ajout de la bibliothèque Junit au projet 
    * Ajout de Git avec une sauvegarde en local et ce premier test de
    fonctionnement

---
+++ calculatrice/src/main/java/theo_le_boulanger/calculatrice/App.java
@@ -0,0 +1,13 @@
+package theo_le_boulanger.calculatrice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
